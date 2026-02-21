f0bf706396f1a8ff716b6b2b824a154d8a942ecf
ABrunton69 <amdrummer2015@gmail.com>
2026-02-21 01:11:56 +0000

    Transmission TODO

---
+++ Classes/Vehicles/VehicleComponents/Transmission.cs
@@ -0,0 +1,13 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Terminal_Engines.Classes.Vehicles.VehicleComponents
+//{
+//    public class Transmission : VehiclePart
+//    {
+//        public 
+//    }
+//}
